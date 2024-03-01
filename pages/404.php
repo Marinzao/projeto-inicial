c67cf297316c5766f44bde950916ffd24856a6d8
Marinzao <almridaguilherme@gmail.com>
2024-03-01 19:54:28 -0300

    Update 404.php

--- pages/404.php
+++ pages/404.php
@@ -1,9 +1,14 @@
-<div class="h-100v d-flex justify-coontent-center align-items-center">
+<?php
+$color = "#bbb";
+?>
+<div class="d-flex align-items-center justify-content-center" style="min-height: 100vh; background-color:<?= $color ?>">
     <div class="container">
         <div class="row">
-            <div class="col-12">
-                <h1 class="text-center">Página não encontrada</h1>
+            <div class="col-6 mx-auto card shadow p-4">
+                <h1> Erro 404 - Página não encontrada</h1>
             </div>
+            </form>
         </div>
     </div>
+</div>
 </div>
